c35eb81e6affa32dc636ccc7c44867bb3690dbfd
Joy-dpaula <joycedpaula136@gmail.com>
2024-08-29 17:28:14 -0300

    get accounts from api

--- components/Content.js
+++ components/Content.js
@@ -1,44 +1,43 @@
+
+import { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import CardAccount from './Card2';
 
 export default function Content() {
 
-    const accounts = [
-        {
-            id: 1,
-            service: "Gmail",
-            imgUrl: "https://cdn-icons-png.flaticon.com/512/281/281769.png",
-            userName: "joycedpaula136@gmail.com"
-        },
-        {
-            id: 1,
-            service: "Github",
-            imgUrl: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
-            userName: "Joy-dpaula"
-        },
-        {
-            id: 1,
-            service: "Instagram",
-            imgUrl: "https://cdn-icons-png.flaticon.com/512/2111/2111463.png",
-            userName: "Joy_dpaula"
-        },
-        {
-            id: 1,
-            service: "X",
-            imgUrl: "https://cdn-icons-png.flaticon.com/256/5968/5968830.png",
-            userName: "jujujoba"
-        },
+    const [accounts, setAccounts] = useState([])
+
+    useEffect(() => {
+
+        const getAccounts = async () => {
+            const response = await fetch('http://localhost:3000/account/list')
+
+            if (response.ok) {
+                const data = await response.json()
+                console.log(data)
+                setAccounts(data.accounts)
+                return
+
+            }
+            console.log('Erro ao carregar accounts')
+            return
+        }
+        getAccounts()
         
-    ]
+    }, [])
+   
 
     return (
         <View style={styles.content}>
+
+
+            { accounts.length === 0 && <Text>Loading...</Text>}
 
             {
                 accounts.map((account) => 
 
                     <CardAccount key={account.id}
-                    service={account.service}imgUrl={account.imgUrl} userName={account.userName}/>
+                    service={account.service}imgUrl={account.logo_image} userName={account.username}/>
                 )
             }
 
